8ccd97a865376357b7dfbddf3f1d45c4dfa82bd3
David Wisseh <wissehdavid@gmail.com>
2023-08-07 13:40:13 -0400

    Add playlists, songs, and playlist_song table
     run ./create.sh to uodate database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,9 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS playlist_song;
+DROP TABLE IF EXISTS songs;
+DROP TABLE IF EXISTS playlists;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +11,30 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+CREATE TABLE playlists(
+	playlist_id Serial PRIMARY KEY,
+	playlist_name VARCHAR(255),
+	dj_id int,
+	special_event VARCHAR(255),
+	host_id int,
+	CONSTRAINT FK_dj_id FOREIGN KEY(dj_id) REFERENCES users(user_id),
+	CONSTRAINT FK_host_id FOREIGN KEY(host_id) REFERENCES users(user_id)
+	
+);
+CREATE TABLE songs(
+	song_id SERIAL PRIMARY KEY,
+	title VARCHAR(255),
+	artist VARCHAR(255),
+	album VARCHAR(255)
+	
+);
+CREATE TABLE playlist_song(
+	playlist_id int ,
+	song_id int,
+	CONSTRAINT FK_playlist_id FOREIGN KEY(playlist_id) REFERENCES playlists(playlist_id),
+	CONSTRAINT FK_song_id FOREIGN KEY(song_id) REFERENCES songs(song_id),
+	CONSTRAINT PK_playlist_song PRIMARY KEY(playlist_id, song_id)
 );
 
 COMMIT TRANSACTION;
